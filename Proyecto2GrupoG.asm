80dfdbf02d8c613c8e06c8d952333670b3675f33
Andrés Ignacio Torres <andresitorresm@gmail.com>
2018-03-30 03:17:01 -0400

    a e s t h e t i c changes

--- Proyecto2GrupoG.asm
+++ Proyecto2GrupoG.asm
@@ -24,24 +24,20 @@
 
 .data
 Display: .space 4096
-Tamano: .word 4096
 Inicio: .word 0
 Letra: .word 0
 Timer: .word 0
 Barra: .word 0
 Ladrillos: .word 128
-T: .word 2
+T: .word 100
 UltimaFila: .word 3968
 Vx: .word 0
 Vy: .word 0
 Px: .word 0
 Py: .word 0
-Azul: .word 0x000077, 0x0000a7, 0x0000f4
 Amarillo: .word 0xFFFF30
-Verde1: .word 0x005000
-Verde2: .word 0x008000
-Verde3: .word 0x00b000
-Verde4: .word 0x00fa00
+Azul: .word 0x000077, 0x0000a7, 0x0000f4
+Verde: .word 0x005000, 0x008000, 0x00b000
 Gris: .word 0x111111
 Negro: .word 0x000000
 endmessage: .asciiz " for Quality!"
@@ -81,82 +77,11 @@ setup:
 	addi $a0, $a0, 1
 	sw $a0, Vy	
 	
-	# Cargamos en $s0 la dirección del Bitmap Display
-	la $s0, Display
-	
-	# Cargamos en $t1 el tamaño en bloques del Display
-	lw $t1, Tamano
-	
-	# Copiamos en $t2 la dirección del Display para pintar
-	move $t2, $s0
-	
-	# Copiamos en los registros $t3 a $t6 los colores para pintar
-	# el tablero de juego
-	lw $t3, Verde1
-	lw $t4, Verde2
-	lw $t5, Verde3
-	
-	# Cargamos los ladrillos. Son 128 ladrillos que hay que colorear
-	# Para poder rellenar 4 hileras
-CargarLadrillos:
-	# Coloreamos de dos tonos los primeros dos ladrillos
-	sw $t3,  0($t2)
-	sw $t4,  4($t2)
-	addiu $t2, $t2, 8 # Dos desplazamos dos bytes en el Display
-	
-	# Nos restan 126 hileras, por lo que iteramos de 6 en 6
-	li $t9, 126
-	
-	cargarLoop:
-	beqz $t9, LadrillosCargados # Si ya coloreamos los 126 ladrillos restantes, saltar
-	sw $t5,  0($t2)
-	sw $t3,  4($t2)
-	sw $t4,  8($t2)
-	sw $t5, 12($t2)
-	sw $t3, 16($t2)
-	sw $t4, 20($t2)
-	
-	addiu $t2, $t2, 24
-	addiu $t9, $t9, -6
-	b cargarLoop
-			
-LadrillosCargados:
-	addiu $t2, $t2, 3328
-	
-	# Pintamos la pelota
-	lw $t3, Amarillo
-	sw $t3, 60($t2)
-	
-	li $t5, 15
-	li $t4, 30
-	
-	# Guardamos la ubicación de la pelota
-	sw $t5, Px
-	sw $t4, Py
-		
-	addiu $t2, $t2, 128
-	
-	# Pintamos la barra
-	lw $t3, Azul
-	lw $t4, Azul+4
-	lw $t5, Azul+8
-	
-	sw $t3, 52($t2)
-	sw $t4, 56($t2)
-	sw $t5, 60($t2)
-	sw $t4, 64($t2)
-	sw $t3, 68($t2)
-	
-	# Guardamos la posición en bits de la barra (dada por su extremo izquierdo)
-	li $t9, 52
-	sw $t9, Barra
-	
 	# Encendemos los bits 1, 8, 16 del registro Status
 	# El 1 para activar Interrupciones
 	# El 8 para pemitir interrupciones de nivel 1
-	# El 16 para encender el Timer
 	mfc0 $a0, $12
-	ori $a0, 0x8101 
+	ori $a0, 0x101 
 	mtc0 $a0, $12
 	
 	# Encendemos el bit 2 del Receiver Control para
@@ -170,17 +95,351 @@ splashScreen:
 	li $v0 4
 	syscall
 	
+	dibujarSplash:
+	# Cargamos en $t3 a $t5 los colores azules
+	lw $t3, Azul
+	lw $t4, Azul+4
+	lw $t5, Azul+8
+
+	# Cargamos en $s0 la dirección del Bitmap Display
+	la $s0, Display
+
+	# Dejamos la primera línea en negro
+	addiu $s0, $s0, 128
+
+	#Pintamos BREAK
+	sw $t3, 4($s0)
+	sw $t3, 8($s0)
+	sw $t3, 12($s0)
+	sw $t3, 24($s0)
+	sw $t3, 28($s0)
+	sw $t3, 32($s0)
+	sw $t3, 44($s0)
+	sw $t3, 48($s0)
+	sw $t3, 52($s0)
+	sw $t3, 64($s0)
+	sw $t3, 76($s0)
+	sw $t3, 88($s0)
+	addiu $s0, $s0, 128
+	
+	sw $t4, 4($s0)
+	sw $t4, 16($s0)
+	sw $t4, 24($s0)
+	sw $t4, 36($s0)
+	sw $t4, 44($s0)
+	sw $t4, 60($s0)
+	sw $t4, 68($s0)
+	sw $t4, 76($s0)
+	sw $t4, 84($s0)
+	addiu $s0, $s0, 128
+
+	sw $t3, 4($s0)
+	sw $t3, 8($s0)
+	sw $t3, 12($s0)
+	sw $t3, 24($s0)
+	sw $t3, 28($s0)
+	sw $t3, 32($s0)
+	sw $t3, 44($s0)
+	sw $t3, 48($s0)
+	sw $t3, 60($s0)
+	sw $t3, 64($s0)
+	sw $t3, 68($s0)
+	sw $t3, 76($s0)
+	sw $t3, 80($s0)
+	addiu $s0, $s0, 128
+
+	sw $t4, 4($s0)
+	sw $t4, 16($s0)
+	sw $t4, 24($s0)
+	sw $t4, 32($s0)
+	sw $t4, 44($s0)
+	sw $t4, 60($s0)
+	sw $t4, 68($s0)
+	sw $t4, 76($s0)
+	sw $t4, 84($s0)
+	addiu $s0, $s0, 128
+
+	sw $t3, 4($s0)
+	sw $t3, 8($s0)
+	sw $t3, 12($s0)
+	sw $t3, 24($s0)
+	sw $t3, 36($s0)
+	sw $t3, 44($s0)
+	sw $t3, 48($s0)
+	sw $t3, 52($s0)
+	sw $t3, 60($s0)
+	sw $t3, 68($s0)
+	sw $t3, 76($s0)
+	sw $t3, 88($s0)
+	addiu $s0, $s0, 128
+
+	# Línea en blanco
+	addiu $s0, $s0, 128
+
+	# Escribimos OUT
+	sw $t4, 80($s0)
+	sw $t4, 92($s0)
+	sw $t4, 104($s0)
+	sw $t4, 112($s0)
+	sw $t4, 116($s0)
+	sw $t4, 120($s0)
+	addiu $s0, $s0, 128
+
+	sw $t3, 76($s0)
+	sw $t3, 84($s0)
+	sw $t3, 92($s0)
+	sw $t3, 104($s0)
+	sw $t3, 116($s0)
+	addiu $s0, $s0, 128
+
+	sw $t4, 76($s0)
+	sw $t4, 84($s0)
+	sw $t4, 92($s0)
+	sw $t4, 104($s0)
+	sw $t4, 116($s0)
+	addiu $s0, $s0, 128
+
+	sw $t3, 76($s0)
+	sw $t3, 84($s0)
+	sw $t3, 92($s0)
+	sw $t3, 104($s0)
+	sw $t3, 116($s0)
+	addiu $s0, $s0, 128
+
+	sw $t4, 80($s0)
+	sw $t4, 96($s0)
+	sw $t4, 100($s0)
+	sw $t4, 116($s0)
+
+	addiu $s0, $s0, 128
+	
+	addiu $t0, $0, 128 
+	sll $t0, $t0, 3
+
+	add $s0, $s0, $t0
+
+	# Cargamos en $t3 a $t5 los colores verdes
+	la $t2, Verde
+	lw $t3, ($t2)
+	lw $t4, 4($t2)
+	lw $t5, 8($t2)
+
+	# Escribimos Press
+	sw $t3, 4($s0)
+	sw $t3, 8($s0)
+	sw $t3, 12($s0)
+	sw $t3, 24($s0)
+	sw $t3, 28($s0)
+	sw $t3, 32($s0)
+	sw $t3, 44($s0)
+	sw $t3, 48($s0)
+	sw $t3, 52($s0)
+	sw $t3, 64($s0)
+	sw $t3, 68($s0)
+	sw $t3, 84($s0)
+	sw $t3, 88($s0)
+	addiu $s0, $s0, 128
+
+	sw $t4, 4($s0)
+	sw $t4, 16($s0)
+	sw $t4, 24($s0)
+	sw $t4, 36($s0)
+	sw $t4, 44($s0)
+	sw $t4, 60($s0)
+	sw $t4, 80($s0)
+	addiu $s0, $s0, 128
+
+	sw $t3, 4($s0)
+	sw $t3, 8($s0)
+	sw $t3, 12($s0)
+	sw $t3, 24($s0)
+	sw $t3, 28($s0)
+	sw $t3, 32($s0)
+	sw $t3, 44($s0)
+	sw $t3, 48($s0)
+	sw $t3, 64($s0)
+	sw $t3, 68($s0)
+	sw $t3, 84($s0)
+	sw $t3, 88($s0)
+	addiu $s0, $s0, 128
+
+	sw $t4, 4($s0)
+	sw $t4, 24($s0)
+	sw $t4, 32($s0)
+	sw $t4, 44($s0)
+	sw $t4, 72($s0)
+	sw $t4, 92($s0)
+	addiu $s0, $s0, 128
+
+	sw $t3, 4($s0)
+	sw $t3, 24($s0)
+	sw $t3, 36($s0)
+	sw $t3, 44($s0)
+	sw $t3, 48($s0)
+	sw $t3, 52($s0)
+	sw $t3, 64($s0)
+	sw $t3, 68($s0)
+	sw $t3, 84($s0)
+	sw $t3, 88($s0)
+	addiu $s0, $s0, 128
+
+	# Saltamos una línea
+	addiu $s0, $s0, 128
+
+	# Escribimos A KEY
+	sw $t4, 44($s0)
+	sw $t4, 68($s0)
+	sw $t4, 80($s0)
+	sw $t4, 88($s0)
+	sw $t4, 92($s0)
+	sw $t4, 96($s0)
+	sw $t4, 104($s0)
+	sw $t4, 112($s0)
+	addiu $s0, $s0, 128
+
+	sw $t3, 40($s0)
+	sw $t3, 48($s0)
+	sw $t3, 68($s0)
+	sw $t3, 76($s0)
+	sw $t3, 88($s0)
+	sw $t3, 104($s0)
+	sw $t3, 112($s0)
+	addiu $s0, $s0, 128
+
+	sw $t4, 40($s0)
+	sw $t4, 44($s0)
+	sw $t4, 48($s0)
+	sw $t4, 68($s0)
+	sw $t4, 72($s0)
+	sw $t4, 88($s0)
+	sw $t4, 92($s0)
+	sw $t4, 104($s0)
+	sw $t4, 112($s0)
+	addiu $s0, $s0, 128
+
+	sw $t3, 40($s0)
+	sw $t3, 48($s0)
+	sw $t3, 68($s0)
+	sw $t3, 76($s0)
+	sw $t3, 88($s0)
+	sw $t3, 108($s0)
+	addiu $s0, $s0, 128
+
+	sw $t4,  40($s0)
+	sw $t4,  48($s0)
+	sw $t4,  68($s0)
+	sw $t4,  80($s0)
+	sw $t4,  88($s0)
+	sw $t4,  92($s0)
+	sw $t4,  96($s0)
+	sw $t4, 108($s0)
+	addiu $s0, $s0, 128
+
 	splashLoop:
 	lw $s7, Letra
 	beqz $s7, splashLoop
 	sw $s0, Letra
 	
-	iniciarJuego:
+dibujarTablero:
+	# Cargamos en $s0 la dirección del Bitmap Display
+	la $s0, Display
+
+	# Reiniciamos el tablero en negro
+	lw $t2, Negro
+	li $t9, 1024
+	loopNegrear:
+	beqz $t9, finNegrear
+	sw $t2,  0($s0)
+	sw $t2,  4($s0)
+	sw $t2,  8($s0)
+	sw $t2, 12($s0)
+	sw $t2, 16($s0)
+	sw $t2, 20($s0)
+	sw $t2, 24($s0)
+	sw $t2, 28($s0)
+	addiu $t9, $t9, -8
+	addiu $s0, $s0, 32
+	b loopNegrear
+
+	finNegrear:
+
+	# Cargamos en $s0 la dirección del Bitmap Display
+	la $s0, Display
+
+	# Copiamos en los registros $t3 a $t6 los colores para pintar
+	# el tablero de juego
+	la $t2, Verde
+	lw $t3, ($t2)
+	lw $t4, 4($t2)
+	lw $t5, 8($t2)
+	
+	# Cargamos los ladrillos. Son 128 ladrillos que hay que colorear
+	# Para poder rellenar 4 hileras
+CargarLadrillos:
+	# Coloreamos de dos tonos los primeros dos ladrillos
+	sw $t3,  0($s0)
+	sw $t4,  4($s0)
+	addiu $s0, $s0, 8 # Nos desplazamos dos bytes en el Display
+	
+	# Nos restan 126 hileras, por lo que iteramos de 6 en 6
+	li $t9, 126
+	
+	cargarLoop:
+	beqz $t9, LadrillosCargados # Si ya coloreamos los 126 ladrillos restantes, saltar
+	sw $t5,  0($s0)
+	sw $t3,  4($s0)
+	sw $t4,  8($s0)
+	sw $t5, 12($s0)
+	sw $t3, 16($s0)
+	sw $t4, 20($s0)
+	
+	addiu $s0, $s0, 24
+	addiu $t9, $t9, -6
+	b cargarLoop
+			
+LadrillosCargados:
+	addiu $s0, $s0, 3328
+	
+	# Pintamos la pelota
+	lw $t3, Amarillo
+	sw $t3, 60($s0)
+	
+	li $t5, 15
+	li $t4, 30
+	
+	# Guardamos la ubicación de la pelota
+	sw $t5, Px
+	sw $t4, Py
+		
+	addiu $s0, $s0, 128
+	
+	# Pintamos la barra
+	lw $t3, Azul
+	lw $t4, Azul+4
+	lw $t5, Azul+8
+	
+	sw $t3, 52($s0)
+	sw $t4, 56($s0)
+	sw $t5, 60($s0)
+	sw $t4, 64($s0)
+	sw $t3, 68($s0)
+	
+	# Guardamos la posición en bits de la barra (dada por su extremo izquierdo)
+	li $t9, 13
+	sw $t9, Barra
+	
+iniciarJuego:	
+	# Encendemos el bit 16 (timer)
+	mfc0 $a0, $12
+	ori $a0, 0x8000 
+	mtc0 $a0, $12
+	
 	# Para imprimir mensajes al presionar teclas (debugging)
 	li $v0, 4 
 	
 	# Ponemos al timer a esperar (part-debugging)
 	lw $a0, T
+	sll $a0, $a0, 4
 	jal esperar
 	
 main:	
@@ -190,6 +449,7 @@ main:
 	jal moverPelota
 		
 	lw $a0, T
+	sll $a0, $a0, 4
 	jal esperar
 	
 	noMover:
@@ -239,7 +499,7 @@ moverIzquierda:
 	lw $t0, Barra
 	beqz $t0, retornarIzq
 	
-	addiu $t0, $t0, -4
+	addiu $t0, $t0, -1
 	sw $t0, Barra
 	jal redibujarBarra
 	
@@ -257,6 +517,7 @@ redibujarBarra:
 	la $t0, Display
 	lw $t1, UltimaFila
 	lw $t2, Barra
+	sll $t2, $t2, 2
 	
 	add $t0, $t0, $t1
 	add $t0, $t0, $t2
@@ -287,7 +548,7 @@ moverDerecha:
 	lw $t0, Barra
 	beq, $t0, 108, retornarDer
 	
-	addiu $t0, $t0, 4
+	addiu $t0, $t0, 1
 	sw $t0, Barra
 	jal redibujarBarra
 	
@@ -314,7 +575,7 @@ moverPelota:
 	b fronteraY0
 	
 	fronteraX128:
-	blt $t0, 32, fronteraY0
+	blt $t0, 31, fronteraY0
 	sub $t2, $0, $t2
 	sw $t2, Vx
 	b fronteraY0
@@ -326,7 +587,7 @@ moverPelota:
 	b mover
 	
 	fronteraYfin:
-	blt $t1, 32, mover
+	blt $t1, 31, mover
 	sub $t3, $0, $t3
 	sw $t3, Vy
 	b mover
@@ -335,55 +596,65 @@ moverPelota:
 	sub $s1, $t0, $t2
 	sub $s2, $t1, $t3
 	
-	jal dibujarPuntoNegro
+	lw $a0, Negro
+	jal dibujarPelotaConColor
 	
 	sw $s1, Px
 	sw $s2, Py
 	
-	jal dibujarPelota
+	lw $a0, Amarillo
+	jal dibujarPelotaConColor
 	
+	jal chequearAzul
+	
+	lw $ra, 0($sp)
+	addiu $sp, $sp, 4
+	
+	jr $ra
+	
+chequearAzul:
+	addiu $sp, $sp, -4
+	sw $ra, 0($sp)
+	
+	lw $t0, Px
+	lw $t1, Py
+	addi $t1, $t1, 1
+	
+	sll $t0, $t0, 7
+	sll $t1, $t1, 4
+	lw $t2, Display
+	add $t2, $t2, $t0
+	add $t2, $t2, $t1
+	
+	lw $t3, 0($t2)
+	
+	beqz $t3, chequearRet
+	
+	# debo dar distitnas velocidades, esto es debgging
+	lw $t2, Vy
+	sub $t2, $0, $t2
+	
+	chequearRet:
 	lw $ra, 0($sp)
 	addiu $sp, $sp, 4
 	
 	jr $ra
 
-dibujarPuntoNegro:
+# Recibe en $a0 un color
+dibujarPelotaConColor:
 	addiu $sp, $sp, -4
 	sw $ra, 0($sp)
 	
 	lw $t0, Px
 	lw $t1, Py
 	
-	lw $t2, Negro
-	mul $t1, $t1, 128
-	mul $t0, $t0, 4
+	sll $t1, $t1, 7 # Multiplicamos por 2^7, es decir, 128 
+	sll $t0, $t0, 2 # Multiplicamos por 2^2, es decir, 4
 	
 	la $t3, Display
 	add $t3, $t3, $t1
 	add $t3, $t3, $t0
-	sw $t2, 0($t3)
-	
-	lw $ra, 0($sp)
-	addiu $sp, $sp, 4
-	
-	jr $ra
-	
-dibujarPelota:
-	addiu $sp, $sp, -4
-	sw $ra, 0($sp)
-	
-	lw $t0, Px
-	lw $t1, Py
-	
-	lw $t2, Amarillo
-	
-	mul $t1, $t1, 128
-	mul $t0, $t0, 4
-	
-	la $t3, Display
-	add $t3, $t3, $t1
-	add $t3, $t3, $t0
-	sw $t2, 0($t3)
+	sw $a0, 0($t3)
 	
 	lw $ra, 0($sp)
 	addiu $sp, $sp, 4
--- README.md
+++ README.md
@@ -3,13 +3,15 @@ bella y sensual sobrenatural
 
 ## What's missing?
 - [ ] Cambiar de posición (reacción) al chocar con bloques
+- [ ] Cambiar de posición (reacción) al chocar con barra
 - [ ] Contar los bloques chocados
-- [ ] Pantalla de inicio
+- [x] Pantalla de inicio
 - [ ] Pantalla de fin won game
 - [ ] Pantalla de fin game over
 - [ ] Llevar control de vidas
 - [ ] Matar con las vidas por underflow
 - [ ] Control del T (uU, lL, incremento)
+- [ ] Que el timer funcione bien, no solo la primer vez
 - [ ] Musiquita
 - [ ] A e s t h e t i c s
 - [ ] Pausar / despausar
